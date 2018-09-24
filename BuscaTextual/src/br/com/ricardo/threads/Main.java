1d334320a764dfe7858516b20a09b77a4e7b9db5
RuntimeMendes <runtime_mendes@live.com>
2018-09-24 00:06:15 -0300

    BuscaTextualThread

---
+++ BuscaTextual/src/br/com/ricardo/threads/Main.java
@@ -0,0 +1,31 @@
+package br.com.ricardo.threads;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		String nome = "DA";
+		
+	  Thread threadAssinaturas1 = new Thread(new TarefaBuscaTextual ("assinaturas1.txt",nome));
+		
+	  Thread threadAssinaturas2 = new Thread(new TarefaBuscaTextual ("assinaturas2.txt",nome));
+		 
+	  Thread threadAutores = new Thread(new TarefaBuscaTextual ("autores.txt",nome));
+	  
+	  //inicializando as threads
+	  threadAssinaturas1.start();
+	  
+	  threadAssinaturas2.start();
+	  
+	  threadAutores.start();
+	  
+	  
+		 	
+		
+		
+		
+
+	}
+
+}
